de3ec0558497fef645ff9671faf01c5fbe244924
Otavio <bomdebike10@gmail.com>
2025-06-11 10:56:11 -0300

    Criando labelLogin

--- ExemploAula/Form1.Designer.cs
+++ ExemploAula/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lbllogin = new Label();
+            SuspendLayout();
+            // 
+            // lbllogin
+            // 
+            lbllogin.AutoSize = true;
+            lbllogin.Location = new Point(135, 41);
+            lbllogin.Name = "lbllogin";
+            lbllogin.Size = new Size(42, 15);
+            lbllogin.TabIndex = 0;
+            lbllogin.Text = "LOGIN";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbllogin);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbllogin;
     }
 }
